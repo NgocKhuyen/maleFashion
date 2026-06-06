d80d513abffa73d359465d942fcc2bce8f96a863
NgocKhuyen <ngockhuyen2k2@gmail.com>
2026-06-06 10:27:15 +0700

    create layout product and product detail

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", [HomeController::class, 'index']);
+// Home
+Route::get("/", [HomeController::class, 'index'])->name('home');
+// Product
+Route::prefix("products")->group(function () {
+    Route::get("/", [ProductController::class, "index"]);
+    Route::get("/{slug}", [ProductController::class, "show"]);
+});
