a87955070110d2546317d75f23bc1273d3dbc370
NgocKhuyen <ngockhuyen2k2@gmail.com>
2026-06-06 11:10:58 +0700

    Create blog page and blog detail page layout

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,11 +10,4 @@ class HomeController extends Controller
         return view('home.index');
     }
 
-     public function about() {
-        return view('home.about');
-    }
-
-     public function contact() {
-        return view('home.contact');
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,21 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 // Home
-Route::prefix('/')->group(function () {
-    Route::get('/', [HomeController::class, 'index'])->name('home');
-    Route::get('/about', [HomeController::class, 'about'])->name('about');
-    Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+// About
+Route::get('/about', [PageController::class, 'about'])->name('about');
+// Contact
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+// Blog
+Route::prefix('blog')->group(function () {
+    Route::get('/', [BlogController::class, 'index'])->name('blog');
+    Route::get('/{slug}', [BlogController::class, 'show'])->name('blog-detail');
 });
 // Product
 Route::prefix('products')->group(function () {
