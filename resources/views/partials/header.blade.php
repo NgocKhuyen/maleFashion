7c3d10e382f629c98596c9d157fec41a794d6d17
NgocKhuyen <ngockhuyen2k2@gmail.com>
2026-06-08 10:48:48 +0700

    repair the header section

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -57,12 +57,12 @@
             <div class="offcanvas__links">
                 <a href="#">Đăng nhập</a>
             </div>
-            
+
         </div>
         <div class="offcanvas__nav__option">
             <a href="#" class="search-switch"><img src={{ asset('img/icon/search.png') }} alt=""></a>
-            <a href="#"><img src={{ 'img/icon/heart.png' }} alt=""></a>
-            <a href="#"><img src={{ 'img/icon/cart.png' }} alt=""> <span>0</span></a>
+            <a href="#"><img src={{ asset('img/icon/heart.png') }} alt=""></a>
+            <a href="#"><img src={{ asset('img/icon/cart.png') }} alt=""> <span>0</span></a>
             <div class="price">$0.00</div>
         </div>
         <div id="mobile-menu-wrap"></div>
@@ -96,7 +96,7 @@
             <div class="row">
                 <div class="col-lg-3 col-md-3">
                     <div class="header__logo">
-                        <a href="{{ route('home') }}"><img src={{ 'img/logo.png' }} alt=""></a>
+                        <a href="{{ route('home') }}"><img src={{ asset('img/logo.png') }} alt=""></a>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6">
@@ -121,24 +121,16 @@
                             <li class="{{ request()->routeIs('contact') ? 'active' : '' }}">
                                 <a href="{{ route('contact') }}">Liên hệ</a>
                             </li>
-                        {{-- <li><a href="#">Pages</a>
-                                <ul class="dropdown">
-                                    <li><a href="./about.html">About Us</a></li>
-                                    <li><a href="./shop-details.html">Shop Details</a></li>
-                                    <li><a href="./shopping-cart.html">Shopping Cart</a></li>
-                                    <li><a href="./checkout.html">Check Out</a></li>
-                                    <li><a href="./blog-details.html">Blog Details</a></li>
-                                </ul>
-                            </li> --}}
                         </ul>
                     </nav>
                 </div>
                 <div class="col-lg-3 col-md-3">
                     <div class="header__nav__option">
-                        <a href="#" class="search-switch"><img src={{ 'img/icon/search.png' }}
+                        <a href="#" class="search-switch"><img src={{ asset('img/icon/search.png') }}
                                 alt=""></a>
-                        <a href="#"><img src={{ 'img/icon/heart.png' }} alt=""></a>
-                        <a href={{ route('cart') }}><img src={{ 'img/icon/cart.png' }} alt=""> <span>0</span></a>
+                        <a href="#"><img src={{ asset('img/icon/heart.png') }} alt=""></a>
+                        <a href={{ route('cart') }}><img src={{ asset('img/icon/cart.png') }} alt="">
+                            <span>0</span></a>
                         <div class="price">$0.00</div>
                     </div>
                 </div>
